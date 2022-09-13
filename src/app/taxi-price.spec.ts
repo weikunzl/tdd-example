6cb3ee75aac9d836d1b2505404a4480c3545011b
KUNWEI <kwei@thoughtworks.com>
2022-09-14 07:54:16 +0800

    clean code

--- src/app/taxi-price.spec.ts
+++ src/app/taxi-price.spec.ts
@@ -1,13 +1,3 @@
-import TaxiService from './taxi.service'
-
 describe('test taxi', () => {
-    it('Given 出租车行驶了1公里(8公里以内)，未发生等待，When 计费，Then 收费2元', () => {
-        const price = new TaxiService().calculatePrice(1);
-        expect(price).toEqual(2)
-    })
 
-    it('Given 出租车恰好行驶了8公里，未发生等待，When 计费，Then 收费16元' , () => {
-        const price = new TaxiService().calculatePrice(8);
-        expect(price).toEqual(16)
-    })
 })
--- src/app/taxi.service.ts
+++
@@ -1,7 +0,0 @@
-export default class TaxiService {
-    private UNIT_PRICE = 2;
-
-    calculatePrice(distance: number): number {
-        return distance * this.UNIT_PRICE
-    }
-}
